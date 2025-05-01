9a8ac608109005c6a43aea18ad64d0a1b8868baf
Ritesh Saini <89265437+PlayerRS21@users.noreply.github.com>
2025-05-01 19:54:55 +0530

    Update Kbc.py

--- Kbc.py
+++ Kbc.py
@@ -8,7 +8,7 @@ def game():
     questions = ["Which command is used to list files and directories in Linux?","Which command is used to print the current working directory?","How do you create a new empty file named example.txt?","Which command is used to display the content of a text file?","How can you move a file named file1.txt to a directory named folder1/?","Which command is used to remove a file?","Which command will create a new directory called myfolder?","How can you see all the running processes on a Linux system?","Which command is used to change file permissions?","How do you copy a file called doc.txt to a directory named backup/?"]
     answers = ["ls","pwd","touch example.txt","cat filename","mv file1.txt folder1/","rm","mkdir myfolder","top","chmod","cp doc.txt backup/"]
     rupees = [10000,20000,30000,40000,50000,60000,70000,80000,90000,100000]
-    obj=open("/home/raja/Documents/VS Code/Codes/Python/Python-codes/python-practice/result.txt","w")
+    obj=open("./result.txt","w")
     i=0
     balance=0
     correct=0
